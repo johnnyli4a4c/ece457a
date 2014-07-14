e69df7c93a7729c3559502df32b06b551e935a97
Nicholas Lun <nmlun@uwaterloo.ca>
2014-07-14 19:05:34 -0400

    Added ability to change direction of cameras in Solution Added ability to supply sections of the map as part of input. Camera coverage cannot cross section boundaries.

--- DetermineCost.m
+++ DetermineCost.m
@@ -1,32 +1,51 @@
-function [SolnCost] = DetermineCost( Cameras, Map, Soln )
+function [SolnCost] = DetermineCost( Cameras, CostMap, BoundaryMap, Soln )
 
 % This function determines cost of solution for placing cameras on a map
 %
 % Inputs:
 % 	Cameras: 2 by n matrix containing coverage of each camera.
-%	Map: Map to be monitored by cameras. Each value in matrix is cost 
-%		 of not covering that position. Ex. A value of 2 has higher cost/higher priority that value of 1
-%	Soln: 2 by n matrix containing positions of cameras
+%	CostMap: Cost map of grid to be monitored by cameras. Each value of position (i,j) in the matrix is cost 
+%		of not covering that position. Ex. A value of 2 has higher cost/higher priority that value of 1
+%	BoundaryMap: Map marking the sections of the map. Value of position (i,j) indicates the section of the map
+%		ex. 1 indicates point is part of section 1, 2 indicates point is part of section 2, etc.
+%	Soln: 3 by n matrix containing position and direction of cameras. Direction is set as 1 for horizontal, 2 for vertical.
 % Outputs:
 % 	SolnCost: Cost of the soln measured as total sum of costs for positions not covered by the cameras.
 
 [CamCount, Dimensions] = size(Cameras);
-[SolnCamCount, Positions] = size(Soln);
+[SolnCamCount, Properties] = size(Soln);
 
 % check number of rows is the same
 if SolnCamCount ~= CamCount
 	error('Soln size is not the same as camera size');
 end
 
+if Properties ~= 3
+	error('Soln needs to be 3 by n matrix');
+end
+
 CostMatrix = Map;
 
 [MaxLength, MaxWidth] = size(CostMatrix);
+[LengthBound, WidthBound] = size(BoundaryMap);
+
+% check that boundary map and cost map have same dimensions
+if MaxLength ~= LengthBound || MaxWidth ~= WidthBound
+	error('Boundary Map does not have same dimensions as cost map');
+end
 
 for Cam = 1:CamCount
 	x = Soln(Cam, 1);
 	y = Soln(Cam, 2);
 	width = Cameras(Cam, 1);
 	length = Cameras(Cam, 2);
+
+	%if direction is vertical swap width and length
+	if Cameras(Cam, 3) == 1
+		temp = width;
+		width = length;
+		length = temp;
+	end
 
 	% for each position on map that the camera covers
 	for i = x:(x + width - 1)
@@ -37,6 +56,15 @@ for Cam = 1:CamCount
 			if i > 0 && i <= MaxWidth && j > 0 && j <= MaxLength
 				CostMatrix(i, j) = 0;
 			end
+
+			% if next iteration will break a boundary between sections, exit out of loop
+			if j+1 <= MaxLength && BoundaryMap(i,j) ~= BoundaryMap(i,j+1)
+				break;
+			end
+		end
+
+		% if i+1 <= MaxWidth && BoundaryMap(i,y) ~= BoundaryMap(i+1, y)
+			break;
 		end
 	end
 end
