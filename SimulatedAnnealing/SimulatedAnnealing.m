c7a0f790707b9394d086baae6af7b2bd4e407190
johnnyli92 <johnny.li@live.ca>
2014-07-24 22:59:05 -0400

    Added freeze detection parameter.

--- SimulatedAnnealing/SimulatedAnnealing.m
+++ SimulatedAnnealing/SimulatedAnnealing.m
@@ -1,60 +1,48 @@
-function [ CamSoln, CamSolnCost, n ] = SimulatedAnnealing( CalcCostFn, CamList, SectionCosts, BoundaryMap, InitialTemp, FinalTemp, alpha)
+function [ CamSoln, CamSolnCost, n ] = SimulatedAnnealing( CalcCostFn, GenInitialSoln, GenNeighbourSoln, CamList, SectionCosts, BoundaryMap, InitialTemp, FinalTemp, alpha, freezeFactor)
     %SIMULATEDANNEALING Summary of this function goes here
     %   Detailed explanation goes here
 
     curTemp = InitialTemp;
+    prevSolnCost = 0;
     n = 0;
+    freezeCount = 0;
 
-    %   Generate initial random solution
-    % Get the number Cameras of dimensions of the BoundaryMap
-    [MaxLength, MaxWidth] = size(BoundaryMap);
-    CamCount = size(CamList, 1);
-
-    %create 3 by n matrix for solution
-    CamSoln = zeros(CamCount, 3);
-
-    % For each camera, generate a random position
-    for i = 1:CamCount
-        x = randi(MaxWidth,1);
-        y = randi(MaxLength,1);
-        direction = randi(2,1);
-        CamSoln(i,:) = [x, y, direction];
-    end
-        
-    % Calculate the cost of the solution
-    [CamSolnCost] = feval(CalcCostFn, CamList, SectionCosts, BoundaryMap, CamSoln);
+    % Generate initial random solution
+    [CamSoln CamSolnCost] = feval(GenInitialSoln, CalcCostFn, CamList, SectionCosts, BoundaryMap);
 
     % Simulated Annealing
     while curTemp > FinalTemp
         
         % move a random camera randomly
-        tmpCamSoln = CamSoln;
-        
-        x = randi(MaxWidth,1);
-        y = randi(MaxLength,1);
-        direction = randi(2,1);
- 
-        tmpCamSoln(randi(CamCount),:) = [x, y, direction];
-        
-        % calculate cost of new solution
-        [tmpCamCost] = feval(CalcCostFn, CamList, SectionCosts, BoundaryMap, tmpCamSoln);
-        
+        [tmpCamSoln tmpCamSolnCost] = feval(GenNeighbourSoln, CalcCostFn, CamList, SectionCosts, BoundaryMap, CamSoln);
+                
         % decide whether to accept new solution
-        if tmpCamCost < CamSolnCost
-            CamSolnCost = tmpCamCost;
+        if tmpCamSolnCost < CamSolnCost
+            CamSolnCost = tmpCamSolnCost;
             CamSoln = tmpCamSoln;
         else
             r = rand;
-            p = exp(((CamSolnCost - tmpCamCost)/curTemp));
+            p = exp(((CamSolnCost - tmpCamSolnCost)/curTemp));
 
             if p > r
-                CamSolnCost = tmpCamCost;
+                CamSolnCost = tmpCamSolnCost;
                 CamSoln = tmpCamSoln;
             end
         end
         
+        if CamSolnCost == prevSolnCost
+            freezeCount = freezeCount + 1;
+        else
+            freezeCount = 0;
+        end
+        
+        if freezeCount > freezeFactor
+            break;
+        end
+        
         n = n + 1;
         curTemp = InitialTemp * alpha^n;
+        prevSolnCost = CamSolnCost;
     end
 end
 
