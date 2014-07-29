6de230bd14b1bd05ac74e9e92743285e8d69799e
Nicholas Lun <nmlun@uwaterloo.ca>
2014-07-29 15:18:04 -0400

    Fixed large running coop

--- submission/TS/runLarge.m
+++ submission/TS/runLarge.m
@@ -3,4 +3,4 @@ load('../largeProblem.mat');
 fprintf('Running Tabu Search for 300 iterations\n');
 Iterations = 300;
 TabuListLength = 15;
-[BestSoln, BestSolnCost] = TabuCamSearch(sections,boundary,cameras,TabuListLength,Iterations,@GenInitialCamSoln,@GetBestNeighbourCamCoopSoln,@DetermineCost)
+[BestSoln, BestSolnCost] = TabuCamSearch(sections,boundary,cameras,TabuListLength,Iterations,@GenInitialCamSoln,@GetBestNeighbourCamSoln,@DetermineCost)
